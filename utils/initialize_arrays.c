e440f2e6801152e93eda0a5014c5e0fbec5a3b40
roger-rab-bit <ruggero-dolzi@virgilio.it>
2023-04-04 18:06:27 +0200

    Add file

---
+++ utils/initialize_arrays.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   initialize_arrays.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rdolzi <rdolzi@student.42roma.it>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/04 17:53:46 by rdolzi            #+#    #+#             */
+/*   Updated: 2023/04/04 18:04:36 by rdolzi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../push_swap.h"
+
+void	initialize_arrays(int *stack_a, int *stack_b,
+		char **argv, int array_len)
+{
+	(void)stack_a;
+	(void)stack_b;
+	array_len = 0;
+	printf(">%s", argv[0]);
+}
