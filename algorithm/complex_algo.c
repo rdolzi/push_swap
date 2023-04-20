5f4021e914ec7b55d97060d1a02e81345f81cc5d
roger-rab-bit <ruggero-dolzi@virgilio.it>
2023-04-20 23:16:10 +0200

    working on V2 t_moves

--- algorithm/complex_algo.c
+++ algorithm/complex_algo.c
@@ -6,35 +6,11 @@
 /*   By: rdolzi <rdolzi@student.42roma.it>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 15:39:16 by rdolzi            #+#    #+#             */
-/*   Updated: 2023/04/20 21:35:07 by rdolzi           ###   ########.fr       */
+/*   Updated: 2023/04/20 23:14:22 by rdolzi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
-
-/*
-void push_top_b(t_stack *st_b, int index)
-{
-	int moves;
-
-	if (index == 0)
-		return;
-	if (index > st_b->size / 2)
-	{
-		moves = st_b->size - index;
-		while (moves-- > 0)
-			rrb(st_b);
-	}
-	else
-	{
-		moves = index;
-		while (moves-- >= 0)
-			rb(st_b);
-	}
-	// printf("\n--POST push_to_b--\n");
-	// test_print_stack(st_b);
-}
-*/
 
 void test_print_moves(t_moves *m)
 {
@@ -42,13 +18,12 @@ void test_print_moves(t_moves *m)
 	printf("dir_a:%d, \ndir_b:%d\n, \nidx_b:%d\n", m->dir_a, m->dir_a, m->idx_b);
 }
 
-// NEW > TO TEST
 void push_top_b(t_stack *st_a, t_stack *st_b, t_moves *moves)
 {
 	// test_print_moves(moves);
 	if (moves->idx_b == 0)
 		return;
-	if (moves->dir_b == 1)
+	else if (moves->dir_b == 1)
 	{
 		while (moves->moves_b-- > 0)
 		{
@@ -78,6 +53,7 @@ void push_top_b(t_stack *st_a, t_stack *st_b, t_moves *moves)
 	// test_print_stack(st_b);
 }
 
+/*
 // simile a moves_a ma esegue lo shuffle dello stack_a, che alla fine
 // si trova nella posizione corretta per inserimento di B[0], che inserisce.
 void push_a(t_stack *stack_a, t_stack *stack_b)
@@ -89,19 +65,12 @@ void push_a(t_stack *stack_a, t_stack *stack_b)
 	i = -1;
 	nbr = stack_b->array[0];
 	moves = 0;
-	// if (!moves_a(stack_a, nbr))
-	// 	{
-	// 		printf("\nin out IF\n");
-	// 		pa(stack_a, stack_b);
-	// 		return ;
-	// 	}
 	while (++i < stack_a->size)
 	{
 		/// printf("\nnbr:%d|array[%d]:%d|array[size - 1]:%d\n", nbr,i, stack_a->array[i], stack_a->array[stack_a->size - 1]);
 		// z./push_swap 6 2 1 3 4 5
 		if (nbr > stack_a->array[i] && nbr < stack_a->array[i + 1])
 		{
-			/// printf("xxx");
 			if (i > stack_a->size / 2)
 			{
 				moves = stack_a->size - i - 1;
@@ -139,8 +108,83 @@ void push_a(t_stack *stack_a, t_stack *stack_b)
 	}
 	// printf("\nafter WHILE\n");
 	pa(stack_a, stack_b);
+}*/
+
+// trova l index del numero immediatamente piu piccolo del nbr
+//  ovvero la differenza deve essere positiva e prendiamo la differenza piu piccola
+int find_min_with(t_stack *stack, int nbr)
+{
+	int i;
+	int distance_first;
+	int distance_second;
+	int min_index;
+
+	i = -1;
+	distance_first = nbr - stack->array[0];
+	min_index = 0;
+	while (++i < stack->size)
+	{
+		distance_second = nbr - stack->array[i];
+		if (distance_second < distance_first)
+		{
+			distance_first = distance_second;
+			min_index = i;
+		}
+	}
+	return (min_index);
 }
 
+// V2
+void push_a(t_stack *stack_a, t_stack *stack_b)
+{
+	int i;
+	int nbr;
+	int moves;
+
+	i = -1;
+	nbr = stack_b->array[0];
+	moves = 0;
+	while (++i < stack_a->size)
+	{
+		/// printf("\nnbr:%d|array[%d]:%d|array[size - 1]:%d\n", nbr,i, stack_a->array[i], stack_a->array[stack_a->size - 1]);
+		// z./push_swap 6 2 1 3 4 5
+		if (nbr > stack_a->array[i] && nbr < stack_a->array[i + 1])
+		{
+			if (i > stack_a->size / 2)
+			{
+				moves = stack_a->size - i - 1;
+				while (moves-- > 0)
+					rra(stack_a);
+			}
+			else
+			{
+				moves = i + 1;
+				while (moves-- > 0)
+					ra(stack_a);
+			}
+			break;
+		}
+		else
+		{ /// find
+			/// printf("yyy");
+			if (find_min_with(stack_a, nbr) > stack_a->size / 2)
+			{
+				moves = stack_a->size - i - 1;
+				while (moves-- > 0)
+					rra(stack_a);
+			}
+			else
+			{
+				moves = i + 1;
+				while (moves-- > 0)
+					ra(stack_a);
+			}
+			break;
+		}
+	}
+	// printf("\nafter WHILE\n");
+	pa(stack_a, stack_b);
+}
 // restituisce index del min value
 int find_min(t_stack *stack)
 {
@@ -197,21 +241,20 @@ void complex_sort(t_stack *stack_a, t_stack *stack_b)
 	// printf("\n--PREstack_b(SIZE:%d)--\n", stack_b->size);
 	// test_print_stack(stack_b);
 	lis(stack_a, stack_b);
-	// printf("\n--POSTstack_a(SIZE:%d)--\n", stack_a->size);
-	// test_print_stack(stack_a);
-	// printf("\n--POSTstack_b(SIZE:%d)--\n", stack_b->size);
-	// test_print_stack(stack_b);
+	printf("\n--POST LIS stack_a(SIZE:%d)--\n", stack_a->size);
+	test_print_stack(stack_a);
+	printf("\n--POST LIS stack_b(SIZE:%d)--\n", stack_b->size);
+	test_print_stack(stack_b);
 	len = stack_b->size;
 	while (len-- > 0)
 	{
 		moves = calculate_moves(stack_a, stack_b, moves);
 		push_top_b(stack_a, stack_b, moves);
 		push_a(stack_a, stack_b);
-		/*
-		index_to_move = calculate_moves(stack_a, stack_b);
-		push_top_b(stack_b, index_to_move);
-		push_a(stack_a, stack_b);
-		*/
+		printf("\n--POST MOVES stack_a(SIZE:%d)--\n", stack_a->size);
+		test_print_stack(stack_a);
+		printf("\n--POST MOVES stack_b(SIZE:%d)--\n", stack_b->size);
+		test_print_stack(stack_b);
 	}
 	sort_a(stack_a);
 }
--- algorithm/greedy_algo.c
+++ algorithm/greedy_algo.c
@@ -6,7 +6,7 @@
 /*   By: rdolzi <rdolzi@student.42roma.it>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/15 15:50:10 by rdolzi            #+#    #+#             */
-/*   Updated: 2023/04/20 21:34:15 by rdolzi           ###   ########.fr       */
+/*   Updated: 2023/04/20 22:44:27 by rdolzi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,59 +50,9 @@
 //       2. Trovare elemento con valore più basso.
 //       3. Muovere in B[0] tale elemento
 //       4. Ruotare A && pa(B)
-/*
+
+/*  il sort funziona but not as intended
 //./push_swap 12 8 -3 -5 20 1 16 2 5 -1 13 14 9 0 10 15 18 17 11 7
-int moves_a(t_stack *stack_a, int nbr)
-{
-	int i;
-	int moves;
-
-	i = -1;
-	moves = 0;
-	while (++i < stack_a->size)
-	{
-		///printf("array[%d]:%d", i, stack_a->array[i]);
-		//printf("nbr:%d|array[i]:%d|array[i + 1]:%d\n", nbr, stack_a->array[i], stack_a->array[i + 1]);
-		if (nbr > stack_a->array[i] && nbr < stack_a->array[i + 1])
-		{
-			///printf("XXX");
-			if ( i > stack_a->size / 2)
-				moves = stack_a->size - i - 1;
-			else
-				moves = i + 1;
-			break;
-		}
-		else
-		{
-			if (nbr < stack_a->array[i] && nbr < stack_a->array[i + 1] && stack_a->array[i] > stack_a->array[i + 1])
-			{
-				///printf("YYY");
-				if ( i > stack_a->size / 2)
-					moves = stack_a->size - i - 1;
-				else
-					moves = i + 1;
-				break;
-			}
-
-		}
-	}
-	return (moves);
-}
-
-int moves_b(int index, int size)
-{
-	int moves;
-
-	moves = 0;
-	//printf("size(b):%d|index:%d", size, index);
-	if (index > size / 2)
-		moves += size - index;
-	else
-		moves += index;
-	return (moves);
-}
-*/
-// NEW > TO TEST
 int moves_a(t_stack *stack_a, int nbr, t_moves *move)
 {
 	int i;
@@ -132,10 +82,66 @@ int moves_a(t_stack *stack_a, int nbr, t_moves *move)
 		else
 		{
 			if (nbr < stack_a->array[i] && nbr < stack_a->array[i + 1] && stack_a->array[i] > stack_a->array[i + 1])
-			{
-				/// printf("YYY");
+			{ // --> verificare con print a cosa punta "i"
+				//./push_swap 11 7 3 113 5 10 -3 -2 14  45 56 150 > test.txt ..dove va 11?
+				// i dovrebbe puntare a 150 per il momento.
+
 				if (i > stack_a->size / 2)
 				{
+					printf("YYY");
+					moves = stack_a->size - i - 1;
+					move->dir_a = 1;
+				}
+				else
+				{
+					moves = i + 1;
+					move->dir_a = -1;
+				}
+				break;
+			}
+		}
+	}
+	return (moves);
+}
+*/
+
+// v2. modifiche da apportare:
+
+int moves_a(t_stack *stack_a, int nbr, t_moves *move)
+{
+	int i;
+	int moves;
+
+	i = -1;
+	moves = 0;
+	while (++i < stack_a->size)
+	{
+		if (nbr > stack_a->array[i] && nbr < stack_a->array[i + 1])
+		{
+			if (i > stack_a->size / 2)
+			{
+				moves = stack_a->size - i - 1;
+				move->dir_a = 1;
+			}
+			else
+			{
+				moves = i + 1;
+				move->dir_a = -1;
+			}
+			break;
+		}
+		else
+		{
+			if (nbr < stack_a->array[i] && nbr < stack_a->array[i + 1] && stack_a->array[i] > stack_a->array[i + 1])
+			{ // --> verificare con print a cosa punta "i"
+				//./push_swap 11 7 3 113 5 10 -3 -2 14  45 56 150 > test.txt ..dove va 11?
+				// i dovrebbe puntare a 150 per il momento.
+				// printf("---dentro caso speciale---\n");
+				// printf("st_a[%d]:%d\n", i, stack_a->array[i]);
+				// printf("nbr:%d||st_a[%d + 1]:%d\n", nbr, i, stack_a->array[i + 1]);
+				if (i > stack_a->size / 2)
+				{
+					printf("YYY");
 					moves = stack_a->size - i - 1;
 					move->dir_a = 1;
 				}
@@ -151,7 +157,6 @@ int moves_a(t_stack *stack_a, int nbr, t_moves *move)
 	return (moves);
 }
 
-// NEW > TO TEST
 int moves_b(int index, int size, t_moves *move)
 {
 	int moves;
@@ -170,45 +175,9 @@ int moves_b(int index, int size, t_moves *move)
 	}
 	return (moves);
 }
-/*
-// ritorna l index dell elemento in stack_b che richiede meno mosse
-int	calculate_moves(t_stack *stack_a, t_stack *stack_b)
-{
-	int i;
-	int m_a;
-	int m_b;
-	int	to_move;
-	// t_stack *moves;
-	int	moves[stack_b->size];
 
-	m_a = 0;
-	m_b = 0;
-	i = -1;
-	// moves = malloc(sizeof(*moves));
-	// moves->array = malloc(stack_b->size * sizeof(int));
-	// moves->size = stack_b->size;
-	while (++i < stack_b->size)
-	{
-		m_a = moves_a(stack_a, stack_b->array[i]);
-		m_b = moves_b(i, stack_b->size);
-		///printf("\nmoves_a:%d |moves_b:%d\n", m_a, m_b);
-		moves[i] = m_a + m_b;
-	}
-	i = -1;
-	to_move = 0;
-	///printf("\n--moves--\n");
-	while (++i < stack_b->size)
-	{
-		if (moves[i] < to_move)
-			to_move = i;
-		///printf("moves[%d]:%d\n", i, moves[i]);
-	}
-	///printf("\nmin index:%d\n", to_move);
-	//test_print_stack(moves);
-	return (to_move);
-}
-*/
-// NEW > TO TEST
+/*
+// non restituisce l elemento con mosse minori ma sempre index 0
 t_moves *calculate_moves(t_stack *stack_a, t_stack *stack_b, t_moves *move)
 {
 	int i;
@@ -225,29 +194,76 @@ t_moves *calculate_moves(t_stack *stack_a, t_stack *stack_b, t_moves *move)
 	{
 		m_a = moves_a(stack_a, stack_b->array[i], move);
 		m_b = moves_b(i, stack_b->size, move);
-		/// printf("\nmoves_a:%d |moves_b:%d\n", m_a, m_b);
+		printf("\nmoves_a:%d |moves_b:%d\n", m_a, m_b);
 		moves[i] = m_a + m_b;
 	}
 	// printf("--PRE--\n");
 	// test_print_moves(move);
 	i = -1;
 	to_move = 0;
-	/// printf("\n--moves--\n");
+	printf("\n--moves--\n");
 	while (++i < stack_b->size)
 	{
-		if (moves[i] < to_move)
+		if (moves[i] < to_move) // -----> dovrebbe essere stack_b->array[to_move] ....
 			to_move = i;
-		/// printf("moves[%d]:%d\n", i, moves[i]);
+		printf("moves[%d]:%d\n", i, moves[i]);
 	}
-	/// printf("\nmin index:%d\n", to_move);
+	// printf("\nmin index:%d\n", to_move);
 	// test_print_stack(moves);
-	// moves_a ritorna il n mosse e setta la direzione
+	//  moves_a ritorna il n mosse e setta la direzione
 	move->moves_a = moves_a(stack_a, stack_b->array[to_move], move);
 	// moves_b ritorna il n mosse e setta la direzione
 	move->moves_b = moves_b(to_move, stack_b->size, move);
 	// setta l index dell elemento di b da spostare
 	move->idx_b = to_move;
-	// printf("\n--post--\n");
+	printf("\n--post--\n");
+	test_print_moves(move);
+	return (move);
+}
+*/
+
+// V2.
+// fix [√]: ora restituisce l elemento con mosse minori anziche sempre index 0
+t_moves *calculate_moves(t_stack *stack_a, t_stack *stack_b, t_moves *move)
+{
+	int i;
+	int m_a;
+	int m_b;
+	int to_move;
+	int moves[stack_b->size];
+
+	m_a = 0;
+	m_b = 0;
+	i = -1;
+	move = malloc(sizeof(*move));
+	while (++i < stack_b->size)
+	{
+		m_a = moves_a(stack_a, stack_b->array[i], move);
+		m_b = moves_b(i, stack_b->size, move);
+		// printf("\nmoves_a:%d |moves_b:%d\n", m_a, m_b);
+		moves[i] = m_a + m_b;
+	}
+	// printf("--PRE--\n");
 	// test_print_moves(move);
+	i = -1;
+	to_move = 0;
+	printf("\n--moves--\n");
+	while (++i < stack_b->size)
+	{
+		if (moves[i] < moves[to_move]) // -----> dovrebbe essere stack_b->array[to_move] ....
+			to_move = i;
+		printf("moves[%d]:%d\n", i, moves[i]);
+	}
+	printf("\nmin index:%d\n", to_move);
+	// test_print_stack(moves);
+	//  moves_a ritorna il n mosse e setta la direzione
+	printf("---NBR:%d---\n", stack_b->array[to_move]);
+	move->moves_a = moves_a(stack_a, stack_b->array[to_move], move);
+	// moves_b ritorna il n mosse e setta la direzione
+	move->moves_b = moves_b(to_move, stack_b->size, move);
+	// setta l index dell elemento di b da spostare
+	move->idx_b = to_move;
+	printf("\n--post--\n");
+	test_print_moves(move);
 	return (move);
 }
